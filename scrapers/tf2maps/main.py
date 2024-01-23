da30aee13e680402aa7067f863cca262d90e4592
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-24 00:37:04 +0100

    refactor: comments & more

--- scrapers/tf2maps/main.py
+++ scrapers/tf2maps/main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 class Map:
+  """Class representing a TF2 map"""
   def __init__(self, name, thumbnail, image, game_mode, release_date):
     self.name = name
     self.thumbnail = thumbnail
@@ -16,6 +18,7 @@ class Map:
     return f"Map: {self.name}\nThumbnail: {self.thumbnail}\nImage: {self.image}\nGame Modes: {self.game_modes}\nRelease Date: {self.release_date}"
   
   def to_dict(self):
+    """Converts the object to a dictionary"""
     return {
       'name': self.name,
       'thumbnail': self.thumbnail,
@@ -23,20 +26,9 @@ class Map:
       'gameModes': self.game_modes,
       'releaseDate': self.release_date
     }
-
-URL = "https://wiki.teamfortress.com/wiki/List_of_maps"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-# Select all <tr> under <tbody> under <table> with class "wikitable sortable grid jquery-tablesorter"
-table = soup.find("table", {"class": "grid"})
-tbody = table.find("tbody")
-trs = tbody.find_all("tr")
-
-hashmap = {}
-
+  
 def convert_to_full_image_url(thumbnail_url):
+  """Converts the url of a thumbnail to the url of the full image"""
   parts = thumbnail_url.split('/')
 
   full_image_parts = [part for i, part in enumerate(parts) if part != 'thumb' and i != len(parts) - 1]
@@ -45,17 +37,36 @@ def convert_to_full_image_url(thumbnail_url):
 
   return full_image_url
 
+# Go to the wiki page
+URL = "https://wiki.teamfortress.com/wiki/List_of_maps"
+page = requests.get(URL)
+
+# Parse the html
+soup = BeautifulSoup(page.content, "html.parser")
+
+# Select all table rows
+table = soup.find("table", {"class": "grid"})
+tbody = table.find("tbody")
+trs = tbody.find_all("tr")
+
+# Initialize empty hashmap. Some maps have multiple rows where 
+# each row represents a different game mode. We want to combine these rows into one map object.
+hashmap = {}
+
+# Loop through each row
 for tr in trs:
+  # Find all table data
   tds = tr.find_all("td")
 
+  # If there are not 7 table data, skip
   if not len(tds) == 7:
     continue
 
   map_name = tds[1].find("a").text
   map_game_mode = tds[2].text.replace("\n", "")
 
+  # If map is already added, add game mode to that map
   if map_name in hashmap.keys():
-    # If game is already added, add game mode to game
     hashmap[map_name].add_game_mode(map_game_mode)
   else:
     # Else, create new game
@@ -63,6 +74,7 @@ for tr in trs:
     map_image = convert_to_full_image_url(map_thumbnail)
     map_release_date = tds[4].find("span")
 
+    # Not all maps have a release date
     if map_release_date:
       map_release_date = map_release_date.text
     else:
@@ -70,10 +82,9 @@ for tr in trs:
 
     hashmap[map_name] = Map(map_name, map_thumbnail, map_image, map_game_mode, map_release_date)
 
-# Write to a json file
-import json
-
+# Convert hashmap to json
 maps_json = json.dumps([map_instance.to_dict() for map_instance in hashmap.values()], indent=4)
 
+# Write json to file
 with open("maps.json", "w") as file:
   file.write(maps_json)
