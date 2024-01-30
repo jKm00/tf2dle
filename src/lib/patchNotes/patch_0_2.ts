c6cc00ee79095d4edc53bb308948d50e79dd1e33
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-30 21:14:18 +0100

    wip: patch 0.2 notes

---
+++ src/lib/patchNotes/patch_0_2.ts
@@ -0,0 +1,13 @@
+export default {
+	version: '0.2.0',
+	date: '',
+	newFeatures: [],
+	improvements: [],
+	bugFixes: [
+		{
+			title: 'Typos in table headers',
+			description: 'Fixed typos in table headers.',
+			gameMode: 'weapon'
+		}
+	]
+};
