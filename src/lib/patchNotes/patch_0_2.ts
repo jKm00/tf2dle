5baed09db3178e19a5ce2d0cbe2a3e60917bed93
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-31 17:42:39 +0100

    update: patch notes

--- src/lib/patchNotes/patch_0_2.ts
+++ src/lib/patchNotes/patch_0_2.ts
@@ -14,6 +14,12 @@ export default {
 			description:
 				'Timer would continue to count down after midnight with negative values. Should now reset as expected.',
 			gameMode: 'all'
+		},
+		{
+			title: 'Challenge completion bug',
+			description:
+				"Previously, when first loading the page it would sometimes tell you that you had completed the challenge when you hand't. This should now be fixed.",
+			gameMode: 'all'
 		}
 	]
 };
