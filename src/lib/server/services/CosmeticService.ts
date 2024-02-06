dd69bdb06706be0064ad6404ed8953047d1c2055
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-06 17:11:59 +0100

    Cosmetic service documentation

--- src/lib/server/services/CosmeticService.ts
+++ src/lib/server/services/CosmeticService.ts
@@ -2,8 +2,11 @@ import type { Cosmetic } from '$lib/types';
 import cosmetics from '$lib/server/data/cosmetics.json';
 import type { CosmeticRepository } from '$lib/server/repositories/CosmeticRepository';
 import { cosmeticRepository } from '$lib/server/repositories/CosmeticRepositoryPrisma';
-import type { DailyCosmetics } from '@prisma/client';
+import type { CosmeticGuessResponse } from '$lib/dtos';
 
+/**
+ * Service for handling cosmetics
+ */
 class CosmeticService {
 	private cosmetics: Cosmetic[];
 	private repo: CosmeticRepository;
@@ -13,10 +16,18 @@ class CosmeticService {
 		this.repo = repo;
 	}
 
+	/**
+	 * Returns all cosmetics
+	 * @returns all cosmetics
+	 */
 	public getCosmetics() {
 		return this.cosmetics;
 	}
 
+	/**
+	 * Returns the cosmetic that is selected for the current day
+	 * @returns cosmetic for the current day
+	 */
 	public async getTodaysCosmetic() {
 		let cosmetic = await this.repo.findTodaysCosmetic();
 
@@ -27,6 +38,10 @@ class CosmeticService {
 		return cosmetic;
 	}
 
+	/**
+	 * Selects a random cosmetic that is used for the current day
+	 * @returns the selected cosmetic
+	 */
 	private async selectRandomCosmetic() {
 		const randomIndex = Math.floor(Math.random() * this.cosmetics.length);
 		const randomRotation = Math.floor(Math.random() * 4) * 90;
@@ -34,6 +49,13 @@ class CosmeticService {
 		return this.repo.saveTodaysCosmetic(this.cosmetics[randomIndex], randomRotation);
 	}
 
+	/**
+	 * Validates a guess
+	 * @param guess the guess to validate
+	 * @param numberOfGuesses the number of tries the user has made
+	 * @returns an object containing information about the guess and if it was correct.
+	 * A hint is provided if the user has made 15 or more guesses
+	 */
 	public async validateGuess(guess: string, numberOfGuesses: number) {
 		const todaysCosmetic = await this.getTodaysCosmetic();
 		const guessedCosmetic = this.cosmetics.find((cosmetic) => cosmetic.name === guess);
