b5a548a54a7a6a1fdbd4505bb7375f75d66a2d6d
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-06-03 00:29:55 +0200

    feat: patch notes

--- src/lib/patchNotes/index.ts
+++ src/lib/patchNotes/index.ts
@@ -5,7 +5,17 @@ import patch_1_1 from './patch_1_1';
 import patch_1_2 from './patch_1_2';
 import patch_1_3 from './patch_1_3';
 import patch_1_4 from './patch_1_4';
+import patch_1_5 from './patch_1_5';
 
-const patchNotes = [patch_1_4, patch_1_3, patch_1_2, patch_1_1, patch_1_0, patch_0_2, patch_0_1];
+const patchNotes = [
+	patch_1_5,
+	patch_1_4,
+	patch_1_3,
+	patch_1_2,
+	patch_1_1,
+	patch_1_0,
+	patch_0_2,
+	patch_0_1
+];
 
 export default patchNotes;
