88f228c8932690fe20d1cb96ba7f17c4ca8bd0bf
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-06-15 15:21:28 +0200

    feat: patch notes

---
+++ src/lib/patchNotes/patch_1_6.ts
@@ -0,0 +1,14 @@
+export default {
+	version: '1.5',
+	date: '15.06.2024',
+	newFeatures: [
+		{
+			title: 'Quick commands',
+			description:
+				'Added a quick command components that allows you to more easily perform actions on the application with keyboard shortcuts. Quick commands can be access either by pressing the button in the top left corner or CTRL/CMD + K on your keyboard!',
+			gameMode: 'All'
+		}
+	],
+	improvements: [],
+	bugFixes: []
+};
