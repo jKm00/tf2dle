e32b901d7fe7079a016ca998b39ea07833ce23b7
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-06-23 15:12:57 +0200

    feat: patch notes

--- src/lib/patchNotes/index.ts
+++ src/lib/patchNotes/index.ts
@@ -7,8 +7,10 @@ import patch_1_3 from './patch_1_3';
 import patch_1_4 from './patch_1_4';
 import patch_1_5 from './patch_1_5';
 import patch_1_6 from './patch_1_6';
+import patch_1_7 from './patch_1_7';
 
 const patchNotes = [
+	patch_1_7,
 	patch_1_6,
 	patch_1_5,
 	patch_1_4,
---
+++ src/lib/patchNotes/patch_1_7.ts
@@ -0,0 +1,14 @@
+export default {
+	version: '1.5',
+	date: '23.06.2024',
+	newFeatures: [
+		{
+			title: 'New game mode: Unusual Effects',
+			description:
+				'A new game mode has been added. <a href="/game-modes/unusual" class="patch-link">Try it out here.</a> The objective is to guess the unusual effect of the item shown on screen.',
+			gameMode: 'Unusuals'
+		}
+	],
+	improvements: [],
+	bugFixes: []
+};
