e2b605f3acd4ebd27b84985eab1be1ef8be8afc1
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-10-18 11:47:31 +0200

    docs: patch notes

--- src/lib/patchNotes/index.ts
+++ src/lib/patchNotes/index.ts
@@ -12,8 +12,10 @@ import patch_1_6_0 from './patch_1_6_0';
 import patch_1_6_1 from './patch_1_6_1';
 import patch_1_7_0 from './patch_1_7_0';
 import patch_1_7_1 from './patch_1_7_1';
+import patch_1_7_2 from './patch_1_7_2';
 
 const patchNotes = [
+	patch_1_7_2,
 	patch_1_7_1,
 	patch_1_7_0,
 	patch_1_6_1,
---
+++ src/lib/patchNotes/patch_1_7_2.ts
@@ -0,0 +1,14 @@
+export default {
+	version: '1.7.2',
+	date: '18.10.2024',
+	newFeatures: [],
+	improvements: [
+		{
+			title: 'Updated randomizer',
+			description:
+				'Noticed that the same items were being selected too frequently. The randomizer has been updated, hoping to fix this issue.',
+			gameMode: 'All'
+		}
+	],
+	bugFixes: []
+};
