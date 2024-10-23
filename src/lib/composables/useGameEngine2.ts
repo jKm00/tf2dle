f415cf83cf664830fb9959f06dab4f0322a99662
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-10-23 16:22:09 +0200

    refactor: (wip) updated version of game engine

---
+++ src/lib/composables/useGameEngine2.ts
@@ -0,0 +1,167 @@
+import type { GuessResponse } from '$lib/dtos';
+import { get, writable } from 'svelte/store';
+import { useLocalStorage } from './useLocalStorage';
+import { useStats } from './useStats';
+import { onMount } from 'svelte';
+import dayjs from '$lib/configs/dayjsConfig';
+import { toast } from 'svelte-sonner';
+
+enum GameState {
+	WON,
+	GUESSING
+}
+
+type GameEvent = {
+	event: 'won' | 'guessed';
+	date: string;
+};
+
+type GameEngineOptions<T> = {
+	victoryMessageDelay: number;
+	onReset?: () => void;
+	onGuess?: (response: T) => void;
+	onWon?: (result: T) => void;
+};
+
+const defaultOptions = {
+	victoryMessageDelay: 3000
+};
+
+export function useGameEngine<T extends GuessResponse>(
+	name: string,
+	options: GameEngineOptions<T>
+) {
+	const state = writable<GameState>(GameState.GUESSING);
+
+	const stats = useStats(name);
+
+	const lastEvent = useLocalStorage<GameEvent | null>(`${name}_last_event`, null);
+	const guesses = useLocalStorage<T[]>(`${name}_guesses`, []);
+	const streak = useLocalStorage<number>(`${name}_streak`, 0);
+
+	const validating = writable<boolean>(false);
+
+	options = { ...defaultOptions, ...options };
+
+	onMount(() => {
+		init();
+	});
+
+	function init() {
+		const _lastEvent = get(lastEvent);
+
+		if (_lastEvent === null) {
+			resetGame();
+			resetStreak();
+			return;
+		}
+
+		const shouldResetStreak =
+			_lastEvent.event !== `won` ||
+			dayjs(_lastEvent.date).isBefore(dayjs.utc().subtract(1, 'day'), 'day');
+
+		if (shouldResetStreak) {
+			resetStreak();
+		}
+
+		const isSameDay = dayjs.utc(_lastEvent.date).isSame(dayjs.utc(), 'date');
+		switch (_lastEvent.event) {
+			case 'won':
+				if (isSameDay) {
+					state.set(GameState.WON);
+				} else {
+					resetGame();
+				}
+				break;
+			case 'guessed':
+				if (isSameDay) {
+					state.set(GameState.GUESSING);
+				} else {
+					resetGame();
+				}
+				break;
+		}
+	}
+
+	function resetGame() {
+		guesses.set([]);
+		state.set(GameState.GUESSING);
+
+		if (options.onReset) {
+			options.onReset();
+		}
+	}
+
+	function resetStreak() {
+		streak.set(0);
+	}
+
+	async function guess(guess: string) {
+		const _state = get(state);
+		const _validation = get(validating);
+
+		if (_state === GameState.WON || _validation) return;
+
+		validating.set(true);
+
+		let result: T | null = null;
+		let error: string | null = null;
+		try {
+			const res = await fetch(`/api/v1/game-modes/${name}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ guess })
+			});
+			const data = (await res.json()) as T;
+
+			if (!res.ok) {
+				error = 'Failed to validate guess. Please try again!';
+			} else {
+				result = data;
+			}
+		} catch (err) {
+			error = 'Failed to validate guess. Please try again!';
+		} finally {
+			validating.set(false);
+		}
+
+		if (error || !result) {
+			toast.error(error || 'Failed to validate guess. Please try again!');
+			return;
+		}
+
+		guesses.update((prev) => [result, ...prev]);
+		if (options.onGuess) {
+			options.onGuess(result);
+		}
+
+		if (result.correct) {
+			won(result);
+		} else {
+			lastEvent.set({ event: 'guessed', date: result.guessedAt });
+		}
+	}
+
+	function won(result: T) {
+		setTimeout(() => {
+			streak.update((prev) => prev + 1);
+			stats.incrementAttempt(get(guesses).length);
+			state.set(GameState.WON);
+
+			if (options.onWon) {
+				options.onWon(result);
+			}
+		}, options.victoryMessageDelay);
+	}
+
+	return {
+		state,
+		stats,
+		guesses,
+		streak,
+		validating,
+		guess
+	};
+}
